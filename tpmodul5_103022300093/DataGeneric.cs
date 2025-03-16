11ad1295b9ad273c7336abb09c1cdc175b10f98b
bintang ridwan p <bintangridwan30@gmail.com>
2025-03-17 01:45:55 +0700

    menambahkan file data generic

---
+++ tpmodul5_103022300093/DataGeneric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_103022300093
+{
+    class DataGeneric
+    {
+        public T Data { get; set; }
+
+        public DataGeneric(T data)
+        {
+            Data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah: {Data}");
+        }
+
+    }
+}
--- tpmodul5_103022300093/Program.cs
+++ tpmodul5_103022300093/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace tpmodul5_103022300093
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGeneric<string> data = new DataGeneric<string>("NIMAnda");
+            data.PrintData();
+        }
+    }
+
+}
